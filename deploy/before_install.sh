0b59581a3e423ec98d41b2ce6ba848c5bbf6cb24
Adam Smolnik <adam@adamsmolnik.com>
2018-02-01 21:36:48 +0100

    Attempt to fix string issue

--- deploy/before_install.sh
+++ deploy/before_install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-spring_profiles_active=$(cat /opt/report-engine/spring_profiles_active)
-export spring_profiles_active=$spring_profiles_active
-rm -rf /opt/report-engine/*
+rm -f /opt/report-engine/*
--- deploy/start.sh
+++ deploy/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
  
+spring_profiles_active=$(cat /opt/report-engine/env-type/spring_profiles_active)
+export spring_profiles_active=$spring_profiles_active
+echo 'export spring_profiles_active=$spring_profiles_active' >> /etc/bashrc
+echo "env="$spring_profiles_active
 /etc/init.d/report-engine start
