5304881a9d07c1963b0f49f63f4f2fd4956757cf
adams <adam@adamsmolnik.com>
2018-12-05 00:11:13 +0100

    Fix deployment scripts

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
  
+export spring_profiles_active=$DEPLOYMENT_GROUP_NAME
+echo 'export spring_profiles_active=$spring_profiles_active' >> /etc/bashrc
+echo "env="$spring_profiles_active
 /etc/init.d/app start
