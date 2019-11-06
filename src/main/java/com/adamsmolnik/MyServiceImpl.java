e7dd04522285e2abfedf57f98a27cc95438673f3
adams <adam@adamsmolnik.com>
2019-11-06 23:21:14 +0100

    Remove WebConfig

--- src/main/java/com/adamsmolnik/MyServiceImpl.java
+++ src/main/java/com/adamsmolnik/MyServiceImpl.java
@@ -2,6 +2,8 @@ package com.adamsmolnik;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,11 @@ import com.amazonaws.xray.spring.aop.XRayEnabled;
 public class MyServiceImpl implements MyService {
 
 	public List<String> getEntities() {
-		return Arrays.asList("1", "2", "3", "4");
+		try {
+			TimeUnit.MILLISECONDS.sleep(new Random().nextInt(3000));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return Arrays.asList("1", "2", "3", "4", "5");
 	}
 }
