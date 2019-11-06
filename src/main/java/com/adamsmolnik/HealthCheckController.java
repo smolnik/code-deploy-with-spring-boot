722165539e5213c930060216d5d7723957fefef9
adams <adam@adamsmolnik.com>
2019-11-06 21:05:26 +0100

    Add xray

--- src/main/java/com/adamsmolnik/HealthCheckController.java
+++ src/main/java/com/adamsmolnik/HealthCheckController.java
@@ -3,7 +3,9 @@ package com.adamsmolnik;
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
 import java.time.Instant;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +16,15 @@ public class HealthCheckController {
 	@Value("${env}")
 	private String env;
 
+	@Autowired
+	private MyService myService;
+
 	@RequestMapping(path = "hc")
 	public String hc() {
 		try {
-			return "Hello Sir Adam from + " + Inet4Address.getLocalHost().getHostAddress() + " +  :) env=" + env + " "
-					+ Instant.now();
+			List<String> entities = myService.getEntities();
+			return "Hello Adam from + " + Inet4Address.getLocalHost().getHostAddress() + " +  :) env=" + env + " "
+					+ Instant.now() + " entities-> " + entities;
 		} catch (UnknownHostException e) {
 			throw new IllegalStateException(e);
 		}
