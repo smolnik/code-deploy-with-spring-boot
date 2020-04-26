46fdd576a75ecfdae1d22be4c1efd3efe22ffbb6
adams <adam@adamsmolnik.com>
2020-04-26 18:25:38 +0200

    Add MainController

---
+++ src/main/java/com/adamsmolnik/MainController.java
@@ -0,0 +1,24 @@
+package com.adamsmolnik;
+
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+import java.time.Instant;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	public String hc() {
+		try {
+			return "Hello here, Adam from " + Inet4Address.getLocalHost().getHostAddress() + " +  :) at " + Instant.now();
+		} catch (UnknownHostException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+}
